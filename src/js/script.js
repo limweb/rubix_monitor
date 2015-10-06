f17d1738114d52900d1a55d570be3630f8bf8652
Shashank <shashanksabhlok95@gmail.com>
2015-10-06 14:00:01 -0400

    Added rubix_monitor

---
+++ src/js/script.js
@@ -0,0 +1,70 @@
+     
+        /*function showDiv() {
+            document.getElementById('tab2').style.display = "block";
+        }*/
+
+
+
+ $(document).ready(function(){
+ 	jQuery('.tabs .tab-links a').on('click', function(e) {
+ 		var currentAttrValue = jQuery(this).attr('href');
+ 		if (currentAttrValue == "#tab2")
+ 		{
+ 			$('#tab2').show().siblings().hide();
+ 			$('#tab1').show().siblings().removeClass('active');
+ 			$('#tab2').parent('li').addClass('active');
+ 		}
+
+    });
+});
+
+
+ jQuery(document).ready(function() {
+    jQuery('.tabs .tab-links a').on('click', function(e)  {
+        var currentAttrValue = jQuery(this).attr('href');
+ 
+        // Show/Hide Tabs
+        jQuery('.tabs ' + currentAttrValue).show().siblings().hide();
+ 
+        // Change/remove current tab to active
+        jQuery(this).parent('li').addClass('active').siblings().removeClass('active');
+ 
+        e.preventDefault();
+    });
+});
+(function() {
+	$('body').on('mouseenter', '[data-toggle="tooltip"]', function( event ) {
+		$(this).tooltip('show');
+	}).on('mouseleave', '[data-toggle="tooltip"]', function( event ) {
+		$(this).tooltip('hide');
+	});
+
+	$.fn.sparkline.defaults.bar.height = 63;
+	$.fn.sparkline.defaults.bar.barWidth = 6;
+	$.fn.sparkline.defaults.bar.barSpacing = 1;
+	$.fn.sparkline.defaults.bar.tooltipClassname = 'jqstooltip';
+	$.fn.sparkline.defaults.bar.tooltipOffsetX = 0;
+	$.fn.sparkline.defaults.bar.tooltipFormat = $.spformat('<div class="tooltip-arrow"></div><div class="tooltip-inner">{{prefix}}{{value}} {{suffix}}</div>');
+	$.fn.sparkline.defaults.bar.colorMap = $.range_map({
+		'0:6': '#10a0de',
+		'6:15': '#7bcc3a',
+		'15:40': '#FFD162',
+		'40:60': '#ff8a00',
+		'60:': '#F74B4B'
+	});
+
+	moment.relativeTimeThreshold('s', 60);
+	moment.relativeTimeThreshold('m', 60);
+	moment.relativeTimeThreshold('h', 24);
+	moment.relativeTimeThreshold('d', 28);
+	moment.relativeTimeThreshold('M', 12);
+
+})();
+
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+ga('create', 'UA-63657296-1', 'auto');
+ga('send', 'pageview');
